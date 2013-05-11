da09ae22b8fd376fe7453ca01031f1a97d76bc80
Prabhu Sunderaraman <iamprabhu@yahoo.com>
2013-05-11 16:38:35 +0530

    HelloWorld

---
+++ HelloWorld.scala
@@ -0,0 +1,24 @@
+println("Hello World")
+println(args(0))//[] in Java
+args.foreach(arg=>println(arg))
+println("*********Normal for loop")
+for(arg<-args){
+	println(arg)
+}
+
+
+/*Scala(ble) language (F#)
+JVM based language
+Functional and OO style (imperative)
+concurrency
+multi-core processor
+
+String class in Java(Immutable)
+Immutable objects in your app
+classes will not have state 
+	only final variables
+
+statically typed
+case sensitive
+semicolon is optional
+*/
