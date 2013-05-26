e03fa0f963cbc0a87ce4e00ef40cdfa62436f867
Prabhu Sunderaraman <iamprabhu@yahoo.com>
2013-05-26 16:15:31 +0530

    Day 01

--- Day01/src/Lists.scala
+++ Day01/src/Lists.scala
@@ -1,10 +1,27 @@
 object Lists {
 
+	//create a function that will give you a list of even numbers 
+    //The function will accept a list of numbers as argument
+  def evenList2(list: List[Int]):List[Int] = {
+    var listtemp:List[Int] = List()
+    list foreach(i=>if(i%2 == 0) listtemp = listtemp :+ i )
+   listtemp
+ }
+  
   //create a function that will give you a list of even numbers starting from 1 to n where n is the 
   //input parameter to the function
   
+  def evenList(n: Int):List[Int] = {
+	  var list:List[Int] = List()
+	  1 to n foreach(i=>if(i%2 == 0) list = list :+ i )
+	  list
+  } 
+  
   def main(args: Array[String]): Unit = {
-    
+    println(evenList2(List(1,2,3,54,5,566,7,78,8,89,909,0,12)))
+    println("********************************")
+    println(evenList(15))
+    println("********************************")
     var lst1:List[Int] = List(1,2,3)//Immutable
     //println(lst1.getClass())
     
